3ddd979d3c40810a831d684ba2b11b8d54f9f6e8
Yukihide Ochi <konayuki0455@gmail.com>
2020-05-04 06:23:45 +0900

    apply .gitignore

--- config/dev.exs
+++ config/dev.exs
@@ -2,10 +2,10 @@ use Mix.Config
 
 # Configure your database
 config :coscul, Coscul.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "coscul",
+  password: "secret",
   database: "coscul_dev",
-  hostname: "localhost",
+  hostname: "postgres",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
